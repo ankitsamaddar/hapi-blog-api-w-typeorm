ebcaf477a460b6dde0fe82f3fabe7c1a715386bf
ankit <95940970+ankitsamaddar@users.noreply.github.com>
2025-03-12 14:38:21 +0530

    implement pagination and query filtering in GET /users endpoint; update user deletion response format

--- controllers/users/users.controller.ts
+++ controllers/users/users.controller.ts
@@ -8,10 +8,62 @@ export const userController = (con: DataSource): Array<ServerRoute> => {
     {
       method: "GET",
       path: "/users",
-      handler: ({query}: Request, h: ResponseToolkit, err?: Error) => {
-        const options = {where: {... query}}
-        if(!query) delete options.where
-        return userRepo.find(options);
+      handler: async ({ query }: Request, h: ResponseToolkit, err?: Error) => {
+        let { perPage, page, ...q } = query;
+        let realPage: number; // Store the offset for the records in the database
+        let realTake: number; // No. of records per page
+        // Parse records per page
+        if (perPage) realTake = +perPage;
+        else {
+          perPage = "10";
+          realTake = 10;
+        }
+        // Parse the current page and calculate the offset in database
+        if (page) realPage = +page === 1 ? 0 : (+page - 1) * realTake;
+        else {
+          page = "1";
+          realPage = 0;
+        }
+
+        // Set query options
+        const findOptions = {
+          take: realTake,
+          skip: realPage,
+          where: { ...q },
+        };
+        if (!q) delete findOptions.where;
+        // Append the query filters
+        const getQuery = () =>
+          Object.keys(q)
+            .map((key: string) => `${key}=${q[key]}`)
+            .join("&");
+        const qParams = getQuery().length === 0 ? "" : `&${getQuery()}`;
+
+        // Get the pagenations
+        const totalRecords = await userRepo.count({ where: findOptions.where });
+        const totalPages = Math.ceil(totalRecords / realTake);
+        const nextPage =
+          +page < totalPages
+            ? `http://localhost:3000/users?perPage=${realTake}&page=${
+                +page + 1
+              }${qParams}`
+            : null;
+        const prevPage =
+          +page > 1
+            ? `http://localhost:3000/users?perPage=${realTake}&page=${
+                +page - 1
+              }${qParams}`
+            : null;
+
+        // Generate the response
+        return {
+          data: await userRepo.find(findOptions),
+          perPage: realTake,
+          page: +page || 1,
+          totalPages: totalPages,
+          next: nextPage,
+          prev: prevPage,
+        };
       },
     },
     {
@@ -79,10 +131,21 @@ export const userController = (con: DataSource): Array<ServerRoute> => {
           // await con.getRepository(PostsEntity).delete({ userId: Number(id) });
 
           // Update the posts records and set the userId to NULL
-          await con.getRepository(PostsEntity).createQueryBuilder().update().set({ userId: null }).where("userId = :id", { id: Number(id) }).execute();
+          await con
+            .getRepository(PostsEntity)
+            .createQueryBuilder()
+            .update()
+            .set({ userId: null })
+            .where("userId = :id", { id: Number(id) })
+            .execute();
 
           await userRepo.delete(id);
-            return h.response({ msg: "User And Records Deleted Successfully!", user: u }).code(200);
+          return h
+            .response({
+              msg: "User And Records Deleted Successfully!",
+              user: u,
+            })
+            .code(200);
         } catch (error) {
           console.error("Error deleting user:", error);
           return h.response({ message: "Internal Server Error" }).code(500);
